47c67356c5cd431a0c3e0acd7bc44437c0455c0a
Mahabub Islam <miprio101@gmail.com>
2023-12-03 14:17:37 +0600

    delete: removed the test/ folder

--- app/models/buy_order.rb
+++ app/models/buy_order.rb
@@ -2,5 +2,5 @@ class BuyOrder < ApplicationRecord
   belongs_to :business
   belongs_to :buyer, class_name: "User"
 
-  enum status: { pending: 'pending', accept: 'accept', reject: 'reject' }
+  enum status: { pending: 'pending', accepted: 'accept', rejected: 'rejected' }
 end
